ef5ed59d144e079e592083df25769150e2992f07
yangyang <yangyang@sjty.com>
2016-05-18 11:46:25 +0800

    Test 0.1

---
+++ bossapp/app/src/main/java/com/sjty/sjty_yy/bossapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.sjty.sjty_yy.bossapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.provider.Settings;
+
+/**
+ * Created by SJTY_YX on 2016/5/5.
+ */
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        System.out.print("Hello");
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ bossapp/app/src/main/java/com/sjty/sjty_yy/bossapp/OutValue.java
@@ -0,0 +1,9 @@
+package com.sjty.sjty_yy.bossapp;
+
+import android.app.Activity;
+
+/**
+ * Created by SJTY_YX on 2016/5/5.
+ */
+public class OutValue extends Activity {
+}
